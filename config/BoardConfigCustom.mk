94bbd4bce566aeea21f67a12635e9d98b83c5321
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    Add BoardConfigQcom.mk inclusion
    Partial cherry-pick of:
    
    vendor: Move QC board variables earlier
    
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- config/BoardConfigCustom.mk
+++ config/BoardConfigCustom.mk
@@ -1,2 +1,7 @@
 include $(CUSTOM_VENDOR_DIR)/config/BoardConfigKernel.mk
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+	include $(CUSTOM_VENDOR_DIR)/config/BoardConfigQcom.mk
+endif
+
 include $(CUSTOM_VENDOR_DIR)/config/BoardConfigSoong.mk
