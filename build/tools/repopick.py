AOSiP/platform_vendor_aosip

repopick: fix handling of manifest commits
In commit aa3e5cd8ec5d40c0667682820aef8b25b86fce72 I addded a check so as to pick
commits meant for the manifest (platform_manifest repository) to .repo/manifests,
not to the path where we sync it, which would be $TOP/manifest.

This was derped as it searched the manifest and found the real path for where
platform_manifest was synced, i.e $TOP/manifest, and it ended up picking the
changes in there, making our earlier commit useless. Make that check the
first, so that it actually works.

Change-Id: I1639107d1718490e7cd45d9e2b563791437e5480
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

build/tools/repopick.py
@@ -355,15 +355,15 @@ if __name__ == '__main__':
         #   - check that the project path exists
         project_path = None
 
-        if item['project'] in project_name_to_data and item['branch'] in project_name_to_data[item['project']]:
+        if item['project'] == 'platform_manifest':
+            project_path = '.repo/manifests'
+        elif item['project'] in project_name_to_data and item['branch'] in project_name_to_data[item['project']]:
             project_path = project_name_to_data[item['project']][item['branch']]
         elif args.path:
             project_path = args.path
         elif args.ignore_missing:
             print('WARNING: Skipping {0} since there is no project directory for: {1}\n'.format(item['id'], item['project']))
             continue
-        elif item['project'] == 'platform_manifest':
-            project_path = '.repo/manifests'
         else:
             sys.stderr.write('ERROR: For {0}, could not determine the project path for project {1}\n'.format(item['id'], item['project']))
             sys.exit(1)

halogenOS/android_vendor_halogenOS

Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

build/tools/repopick.py
@@ -351,15 +351,15 @@ if __name__ == '__main__':
         #   - check that the project path exists
         project_path = None
 
-        if item['project'] in project_name_to_data and item['branch'] in project_name_to_data[item['project']]:
+        if item['project'] == 'manifest':
+            project_path = '.repo/manifests'
+        elif item['project'] in project_name_to_data and item['branch'] in project_name_to_data[item['project']]:
             project_path = project_name_to_data[item['project']][item['branch']]
         elif args.path:
             project_path = args.path
         elif args.ignore_missing:
             print('WARNING: Skipping {0} since there is no project directory for: {1}\n'.format(item['id'], item['project']))
             continue
-        elif item['project'] == 'manifest':
-            project_path = '.repo/manifests'
         else:
             sys.stderr.write('ERROR: For {0}, could not determine the project path for project {1}\n'.format(item['id'], item['project']))
             sys.exit(1)