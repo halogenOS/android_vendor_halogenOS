PotatoProject/vendor_potato

Add Google Sans font
Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>
Change-Id: Icbd0f342d8f7e5366df37fabf0cbf8bf3a9986c2

config/fonts.mk
@@ -1,0 +1,10 @@
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/aosip/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/aosip/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/aosip/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/aosip/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/aosip/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/aosip/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+ADDITIONAL_FONTS_FILE := vendor/aosip/fonts/google-sans.xml

halogenOS/android_vendor_halogenOS

config/fonts.mk
@@ -1,0 +1,10 @@
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/potato/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/potato/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/potato/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/potato/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/potato/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/potato/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+ADDITIONAL_FONTS_FILE := vendor/potato/fonts/google-sans.xml