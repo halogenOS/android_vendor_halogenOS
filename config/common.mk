PotatoProject/vendor_potato

potato: Update and move bootanimation to prebuilts
- Given the complexity of the new anim, there's a half resolution (perf)
  version that older devices can opt into using.

Change-Id: Ic14f29101a480f4d0341fa0e435d746b15e9d980

config/common.mk
@@ -24,8 +24,13 @@ PRODUCT_COPY_FILES += \
 endif
 
 # Bootanimation
+ifeq ($(BOOTANIM_PERF), true)
 PRODUCT_COPY_FILES += \
-    vendor/potato/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/potato-prebuilts/bootanimation/bootanimation-perf.zip:system/media/bootanimation.zip
+else
+PRODUCT_COPY_FILES += \
+    vendor/potato-prebuilts/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \

halogenOS/android_vendor_halogenOS

config/common.mk
@@ -24,8 +24,13 @@ PRODUCT_COPY_FILES += \
 endif
 
 # Bootanimation
+ifeq ($(TARGET_BOOTANIM_LOW_RES), true)
 PRODUCT_COPY_FILES += \
-    vendor/potato/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/potato-prebuilts/bootanimation/bootanimation-half.zip:system/media/bootanimation.zip
+else
+PRODUCT_COPY_FILES += \
+    vendor/potato-prebuilts/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \