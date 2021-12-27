be5d9b0ec1a390a3276f9bf45725fba2f54ba024
Simão Gomes Viana <devel@superboring.dev>
2021-12-27 23:46:33 +0100

    fixes
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,8 @@
 
-script_dir=$(dirname "$0")
-export CUSTOM_VENDOR_DIR="$script_dir"
+if [ -z "$CUSTOM_VENDOR_DIR" ]; then
+    script_dir=$(dirname "$0")
+    export CUSTOM_VENDOR_DIR="$script_dir"
+fi
 
 source "$CUSTOM_VENDOR_DIR/build/envsetup.sh"
 
@@ -9,5 +11,7 @@ if [ -f $XOS_TOOLS_SCRIPT ]; then
     source $XOS_TOOLS_SCRIPT
 fi
 
-export TOP="$(pwd)"
+if [ -z "$TOP" ]; then
+    export TOP="$(pwd)"
+fi
 
