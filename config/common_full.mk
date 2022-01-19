dbb0d0198306ca980e1514a54535155969c16c7c
Michael Bestas <mkbestas@gmail.com>
2022-01-20 00:17:18 +0200

    config: Build font overlays again
    Change-Id: Ibf6ea9645bbe352a430636c10c3e6e91ac79b99c
    Signed-off-by: Simao Gomes Viana <simao@halogenos.org>

--- config/common_full.mk
+++ config/common_full.mk
@@ -7,3 +7,14 @@ PRODUCT_PACKAGES += \
 	Jelly
 
 PRODUCT_SIZE := full
+
+# Include {Lato,Rubik} fonts
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+# Fonts
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    FontLatoOverlay \
+    FontRubikOverlay
+
