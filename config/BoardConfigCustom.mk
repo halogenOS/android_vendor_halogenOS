a67314fdad7681f3dcffbd62a6a3583ab0eda5c2
Simão Gomes Viana <simao@halogenos.org>
2022-02-24 00:40:26 +0100

    Move QCOM hw thing to BoardConfigCustom

--- config/BoardConfigCustom.mk
+++ config/BoardConfigCustom.mk
@@ -1,2 +1,8 @@
 include $(CUSTOM_VENDOR_DIR)/config/BoardConfigKernel.mk
+
+BOARD_USES_QCOM_HARDWARE ?= $(PRODUCT_USES_QCOM_HARDWARE)
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include $(CUSTOM_VENDOR_DIR)/config/ProductConfigQcom.mk
+endif
+
 include $(CUSTOM_VENDOR_DIR)/config/BoardConfigSoong.mk
