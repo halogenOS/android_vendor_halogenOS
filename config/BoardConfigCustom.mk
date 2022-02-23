6c540c387b2a4cd7b2a5cfec98446197f0bd82c3
Simão Gomes Viana <simao@halogenos.org>
2022-02-24 00:11:06 +0100

    Revert "Add BoardConfigQcom.mk inclusion"
    This reverts commit 94bbd4bce566aeea21f67a12635e9d98b83c5321.

--- config/BoardConfigCustom.mk
+++ config/BoardConfigCustom.mk
@@ -1,7 +1,2 @@
 include $(CUSTOM_VENDOR_DIR)/config/BoardConfigKernel.mk
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-	include $(CUSTOM_VENDOR_DIR)/config/BoardConfigQcom.mk
-endif
-
 include $(CUSTOM_VENDOR_DIR)/config/BoardConfigSoong.mk
