967e27c4d5e4987aad61dc610e5c77815564ba75
Simao Gomes Viana <simao@halogenos.org>
2022-07-13 23:41:16 +0200

    config: Build SourceSansPro overlay

--- config/common_full.mk
+++ config/common_full.mk
@@ -11,10 +11,12 @@ PRODUCT_SIZE := full
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/source-sans-pro/fonts.mk)
 
 # Fonts
 PRODUCT_PACKAGES += \
     fonts_customization.xml \
     FontLatoOverlay \
-    FontRubikOverlay
+    FontRubikOverlay \
+    FontSourceSansProOverlay
 
